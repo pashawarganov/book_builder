7b3c0f0043f34d9df6185c3cadbd697bcb027ebe
Pasha <pavik.varganov@gmail.com>
2025-03-20 22:20:53 +0200

    add 'Google Translate' method

--- translate_text.py
+++ translate_text.py
@@ -1,12 +1,15 @@
+from google.cloud import translate_v2 as google_translate
 from groq import Groq
 
 from settings import settings
 
-client = Groq(api_key=settings.GROQ_API_KEY)
-model1 = "llama3-8b-8192"
 
-def translate(text):
-    response = client.chat.completions.create(
+client_google = google_translate.Client()
+client_groq = Groq(api_key=settings.GROQ_API_KEY)
+
+
+def translate_qroq(text):
+    response = client_groq.chat.completions.create(
         messages=[
             {
                 "role": "user",
@@ -18,13 +21,22 @@ def translate(text):
                     """
             }
         ],
-        model=model1
+        model="llama3-8b-8192"
     )
 
     return response.choices[0].message.content
-# g-mini
+
+
+def translate_google(text):
+    result = client_google.translate(text, target_language="uk")
+    return result["translatedText"]
+
 
 if __name__ == "__main__":
     with open("t.txt", "r") as file:
         chapter1 = file.read()
-    print(translate(chapter1))
+
+    translated_chapter = translate_google(chapter1)
+    with open("new.txt", "w") as file:
+        file.write(translated_chapter)
+        print("File 'new.txt' created successfully.'")
