59366b7999ddb1b7422286f4ab26517b153b8104
Pasha <pavik.varganov@gmail.com>
2025-03-20 21:57:38 +0200

    add new var to settings

--- settings.py
+++ settings.py
@@ -8,6 +8,8 @@ load_dotenv()
 class Settings:
     PROJECT_NAME: str = "Book Builder"
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
+    GPT_API_KEY: str = os.getenv("GPT_API_KEY")
+    GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
 
 
 settings = Settings()
